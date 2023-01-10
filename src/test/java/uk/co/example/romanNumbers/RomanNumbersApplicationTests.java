735f20f749541ffc50ad2a485a8525c05e0e40fd
MatthewCarey <mcarey@kidsloop.live>
2023-01-10 00:54:35 +0000

    coverage up to 100%

--- src/test/java/uk/co/example/romanNumbers/RomanNumbersApplicationTests.java
+++ src/test/java/uk/co/example/romanNumbers/RomanNumbersApplicationTests.java
@@ -11,8 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -77,6 +76,13 @@ class RomanNumbersApplicationTests {
 				.andDo(print()).andExpect(status().isOk())
 				.andExpect(jsonPath("$.lowerLimit").value("1"))
 				.andExpect(jsonPath("$.upperLimit").value(Long.valueOf(upperLimit).toString()));
+	}
+
+	@Test
+	public void getHome() throws Exception{
+		this.mockMvc.perform(get("/"))
+				.andDo(print()).andExpect(status().isOk())
+						.andExpect(model().size(0));
 	}
 }
 
